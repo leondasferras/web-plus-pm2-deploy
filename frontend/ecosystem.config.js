996b69f8c7b7a70089061fb35403909ad01bab6e
leondasferras <90497102+leondasferras@users.noreply.github.com>
2023-04-09 19:13:42 +0500

    add deploy config

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,30 @@
+require('dotenv').config({ path: 'env.deploy'});
+
+const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_REF = 'origin/master', DEPLOY_REP, DEPLOY_PATH } = process.env;
+
+module.exports = {
+  apps : [{
+    name   : "frontend",
+    script : "./build/index.html"
+  }],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REP,
+      path: DEPLOY_PATH,
+      'post-deploy': 'cd frontend && npm i && npm run build',
+    },
+  },
+
+
+
+
+
+
+
+
+
+}
